5be400ab301bd9c01cb350c0795d05ed8e0521ff
Jon-nas <jns.lima@outlook.com>
2024-05-30 10:56:33 -0300

    exercicio de funcoes predefinidas

---
+++ Modularização/exercicio3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{    
+    /* imprime o numero flutuante com várias formatações */
+    printf("%8.2f\n", 12.54321);
+    printf("%8.2f\n", 123.54321);
+    printf("%8.2f\n", 1234.54321);
+    printf("%8.2f\n", 12345.54321);
+
+    return 0;
+}
