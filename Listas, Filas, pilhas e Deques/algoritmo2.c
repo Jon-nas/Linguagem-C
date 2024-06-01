1546cddd1077d545db34dac4dc860ecd73b0b845
Jon-nas <jns.lima@outlook.com>
2024-06-01 10:46:48 -0300

    insercao

---
+++ Listas, Filas, pilhas e Deques/algoritmo2.c
@@ -0,0 +1,8 @@
+// Inserção
+
+int inserir(novo_elemento)
+    se busca(novo_elemento.chave) == n + 1
+        Lista[ n + 1 ] == novo_elemento
+        n = n + 1
+        retornar 1
+    senão retornar -1
