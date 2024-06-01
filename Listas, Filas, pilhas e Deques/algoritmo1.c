bd6d9cccc41cabd797114159460af7dbbb7030c6
Jon-nas <jns.lima@outlook.com>
2024-06-01 10:42:32 -0300

    busca

---
+++ Listas, Filas, pilhas e Deques/algoritmo1.c
@@ -0,0 +1,8 @@
+// Busca
+
+int buscar ( chave )
+ 	se n > 0
+ 		para i = 1 até i <= n
+ 			se Lista [ i ].chave == chave
+ 			retornar i
+ 	retornar n + 1
--- Listas, Filas, pilhas e Deques/codigo1.c
+++ Listas, Filas, pilhas e Deques/codigo1.c
@@ -1,15 +1,6 @@
 // Alocação sequencial na linguagem C.
-#include <stdio.h>
-#include<conio.h>
-
-int x, y, z;
-
-int main()
-{
-    int vetor[10];
-    int a = 50;
-    vetor[3] = a;
-
-    printf("%d", &vetor);
-    return(0);
-}
+[...]
+ int vetor [ 10 ];
+ int a = 50;
+ vetor [ 3 ] = a;
+ [...]
