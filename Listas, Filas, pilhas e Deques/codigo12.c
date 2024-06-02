4d83001235668e7b65c8621fdb2481c9b36376b0
Jon-nas <jns.lima@outlook.com>
2024-06-01 23:05:15 -0300

    funcao de desempilhamento

---
+++ Listas, Filas, pilhas e Deques/codigo12.c
@@ -0,0 +1,20 @@
+// Função de desempilhamento.
+
+int pop (void)
+[
+    No *aux;
+    Elemento elemento_recuperado;
+    if (topo != NULL)
+    {
+        elemento_recuperado = topo->elemento;
+        aux = topo;
+        topo = topo->prox;
+        free (aux);
+        return elemento_recuperado;
+    }
+
+    else
+    [
+        return NULL;
+    ]
+]
