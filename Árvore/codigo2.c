d88363f1063c13e21563d63d80562c9934d4185a
Jon-nas <jns.lima@outlook.com>
2024-06-03 14:21:38 -0300

    Estruturas de no arvore ternaria e generica m-aria

---
+++ Árvore/codigo2.c
@@ -0,0 +1,9 @@
+// Estrutura do nó de uma árvore ternária.
+
+struct No
+{
+    int chave;
+    No *filho_esq;
+    No *filho_meio;
+    No *filho_dir;
+}
---
+++ Árvore/codigo3.c
@@ -0,0 +1,9 @@
+// Estrutura genérica do nó de uma árvore m-ária.
+
+struct No
+{
+    <tipo> chave;
+    No *filho_1;
+    No *filho_2;
+    [...] No *filho_m;
+}
