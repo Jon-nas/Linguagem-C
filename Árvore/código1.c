704732ec27bbd30308c318c5aea099504d45c30f
Jon-nas <jns.lima@outlook.com>
2024-06-03 14:15:09 -0300

    Funcao de insercao de no filho na subarvore direita

---
+++ Árvore/código1.c
@@ -0,0 +1,11 @@
+// representação computacional de árvore.
+
+int insere_filho_direita(char chave, int pai)
+{
+    if (arvore[pai] == NULL)
+    {
+        printf( “Erro : noh pai inexistente !” ) return 0; // falha
+    }
+    else
+        arvore[(pai * 2) + 2] = chave return 1; // sucesso
+}
