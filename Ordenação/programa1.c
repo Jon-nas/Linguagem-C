08dc4320f7f688f15f1c700dd2e96fe4736bc892
Jon-nas <jns.lima@outlook.com>
2024-06-02 21:24:15 -0300

    metodo da bolha

---
+++ Ordenação/programa1.c
@@ -0,0 +1,24 @@
+// Metódo da bolha (bubble sort)
+
+void bolha (int *v)
+{
+	int troca=1;
+	int i=0;
+	int aux;
+	while (troca)
+	{
+		troca = 0;
+		while (i < TAMANHO - 1)
+		{
+			if (v[i] > v[i+1])
+			{
+				aux = v[i];
+				v[i] = v[i+1];
+				v[i+1] = aux;
+				troca = 1;
+			}
+			i++;
+		}
+		i = 0;
+	}
+}
