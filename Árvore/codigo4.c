9f80d308f3dab939e27267073584b9d9bd409adc
Jon-nas <jns.lima@outlook.com>
2024-06-03 14:26:27 -0300

    Funcao insercao de no filho na subarvore direita(alocação encadeada)

---
+++ Árvore/codigo4.c
@@ -0,0 +1,12 @@
+// Função de inserção de nó filho na subárvore direita (alocação encadeada).
+
+int insere_filho_direita(No novo_no, No *pai)
+{
+    if (pai == NULL)
+    {
+        printf( “Erro : noh pai inexistente !” ) return 0; // falha
+    }
+    else
+        pai->filho_dir = novo_no;
+    return 1; // sucesso
+}
